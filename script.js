f8e57c10af402d891453ea88f11087ca14536790
Bhushan <mbhushan4545@gmail.com>
2023-01-28 19:09:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+let string = "";
+let buttons = document.querySelectorAll('.button');
+
+
+
+
+
+
+
+
+
+
+
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+
+   
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'de'){
+      string = string.slice(0,-1)
+
+      document.querySelector('input').value = string;
+     }
+
+     
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
+
+
+
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+// const keys = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
